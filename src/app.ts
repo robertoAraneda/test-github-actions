69e44e4dc5aab85795a205cc586ffa9a334e9bf5
Roberto Araneda Espinoza <robaraneda@gmail.com>
2024-07-14 11:35:26 -0400

    add infra

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,16 @@ app.get("/", (req: Request, res: Response) => {
  res.status(200).json("Hello from the server!!!");
 });
 
+app.get("/api/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Server is running",
+    timestamp: new Date().toISOString(),
+    hrtime: process.hrtime(),
+    cpuUsage: process.cpuUsage(),
+    memoryUsage: process.memoryUsage(),
+  });
+});
+
 app.listen(4000, () => {
  console.log(`App is listening on port 4000`);
 });
